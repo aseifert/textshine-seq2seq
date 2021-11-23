c5d96a73b122dce77e8ac82dc2b3ac2bd384bc3c
Alexander Seifert <4944799+aseifert@users.noreply.github.com>
2021-11-23 23:20:19 +0100

    small refactoring

--- apps/main/app.py
+++ apps/main/app.py
@@ -1,6 +1,7 @@
 import time
 
 import errant
+import spacy
 import streamlit as st
 from happytransformer import HappyTextToText, TTSettings
 
@@ -24,30 +25,10 @@ def get_annotator(lang: str):
     return errant.load(lang)
 
 
-st.title("🤗 Writing Assistant")
-st.markdown(
-    """This writing assistant will proofread any text for you! See my [GitHub repo](https://github.com/aseifert/hf-writing-assistant) for implementation details."""
-)
-
-checkpoint = st.selectbox("Choose model", checkpoints)
-happy_tt = get_model(checkpoint)
-annotator = get_annotator("en")
-args = TTSettings(num_beams=5, min_length=1, max_length=1024)
-
-default_text = "A dog is bigger then mouse."
-default_text = "A dog is bigger then mouse."
-input_text = st.text_area(
-    label="Original text",
-    value=default_text,
-    placeholder="Enter your text here",
-)
-button = st.button("✍️ Check")
-
-
-def output(input_text):
+def output(model, args, annotator, input_text):
     with st.spinner("Checking for errors 🔍"):
         prefixed_input_text = "Grammar: " + input_text
-        result = happy_tt.generate_text(prefixed_input_text, args=args).text
+        result = model.generate_text(prefixed_input_text, args=args).text
 
         try:
             st.success(result)
@@ -58,7 +39,37 @@ def output(input_text):
             st.stop()
 
 
-start = time.time()
-output(input_text)
-st.write("---")
-st.text(f"Built by Team Writing Assistant ❤️ – prediction took {time.time() - start:.2f}s")
+def main():
+    st.title("🤗 Writing Assistant")
+    st.markdown(
+        """This writing assistant will proofread any text for you! See my [GitHub repo](https://github.com/aseifert/hf-writing-assistant) for implementation details."""
+    )
+
+    download_spacy_model()
+    annotator = get_annotator("en")
+    checkpoint = st.selectbox("Choose model", checkpoints)
+    model = get_model(checkpoint)
+    args = TTSettings(num_beams=5, min_length=1, max_length=1024)
+
+    default_text = "A dog is bigger then mouse."
+    default_text = "it gives him many apprtunites in the life, and i think that being knowledge person is a very wouderful thing to have so we can spend our lives in a successful way and full of happenis."
+    input_text = st.text_area(
+        label="Original text",
+        value=default_text,
+    )
+
+    start = None
+    if st.button("✍️ Check"):
+        start = time.time()
+        output(model, args, annotator, input_text)
+
+    st.write("---")
+    st.markdown(
+        f"Built by [@aseifert](https://twitter.com/therealaseifert) during the HF community event – [GitHub repo](https://github.com/aseifert/hf-writing-assistant) – Team Writing Assistant"
+    )
+    if start is not None:
+        st.text(f"prediction took {time.time() - start:.2f}s")
+
+
+if __name__ == "__main__":
+    main()
